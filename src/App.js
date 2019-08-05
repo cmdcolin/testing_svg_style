421d964cdb42a5804c1a99c89ac10f5e66f9c74f
Colin <colin.diesh@gmail.com>
2019-08-05 19:48:17 -0400

    Testing svg

--- src/App.js
+++ src/App.js
@@ -5,20 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg>
+        <rect data-testid="myrect" style={{fill:'green'}} x="0" y="0" width="100" height="100"/>
+      </svg>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {render} from 'react-testing-library'
+import 'jest-dom/extend-expect'
+
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    const {getByTestId} = render(<App />)
+    const x = getByTestId('myrect')
+    expect(x).toHaveStyle('fill:green')
+    expect(x).not.toHaveStyle('fill:red')
 });
